b0737d741ed2ee96c90cc71335eb1eac4534ccaf
emilianospinoso <emilianospinoso@gmail.com>
2023-11-26 09:32:10 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int[] nums = new int[]{4, 1, 2, 1, 2};
+        System.out.println(singleNumber(nums));
+    }
+
+    public static int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int i = 0;
+        while (i < nums.length - 1) {
+            if (nums[i] == nums[i + 1]) {
+                i += 2;
+            } else {
+                return nums[i];
+            }
+        }
+
+        return nums[i];
+    }
+}
